2632d5c5277aee6fef53c492798e2b2472e13578
blueZhang <2029379603@qq.com>
2015-10-11 09:29:07 +0800

    init

--- app/src/main/java/com/bluezhang/picasso_1010_1930_work/fragment/IndexVerticalFragment.java
+++ app/src/main/java/com/bluezhang/picasso_1010_1930_work/fragment/IndexVerticalFragment.java
@@ -143,10 +143,6 @@ public class IndexVerticalFragment extends Fragment {
                 datas.addAll(MV.getResults());
                 adapter.notifyDataSetChanged();
 
-
-
-
-
             }
         });
     }
